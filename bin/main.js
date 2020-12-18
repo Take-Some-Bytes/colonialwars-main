bc8c41482ee0f03d724e566fa552ea7ba1760d1c
Take-Some-Bytes <57299445+Take-Some-Bytes@users.noreply.github.com>
2020-12-17 20:41:03 -0800

    Used graceful shutdown handlers

--- bin/main.js
+++ bin/main.js
@@ -5,6 +5,10 @@
 const ColonialwarsManager = require('../')
 const utils = require('./bin-utils/utils')
 const args = utils.parseArgs(process.argv)
+const {
+  shutDown,
+  handleUncaughtEx
+} = require('./shutdown')
 let confFile = ''
 
 if (args.keyedValues.confFile) {
@@ -15,7 +19,8 @@ process.title = 'colonialwars-main'
 ;(async () => {
   const cwManager = await ColonialwarsManager.create(confFile)
   await cwManager.addAppProcess()
-  process.on('SIGINT', signal => {
-    cwManager.stopAll(signal)
-  })
+  process.on('SIGINT', shutDown.bind(null, cwManager))
+  process.on('SIGTERM', shutDown.bind(null, cwManager))
+  process.on('uncaughtException', handleUncaughtEx.bind(null, cwManager))
+  process.on('unhandledRejection', handleUncaughtEx.bind(null, cwManager))
 })()
